7f18dc3c3f6027cb7e24146a99ba8f7146bfe37f
blackcoat77 <kimanovic@gmail.com>
2017-02-07 17:09:54 -0500

    Add Rmagic gem instead miniMagick gem, update avatar uploader with crop method

--- Gemfile
+++ Gemfile
@@ -22,7 +22,7 @@ gem 'coffee-rails', '~> 4.2'
 # gem 'therubyracer', platforms: :ruby
 
 gem 'carrierwave'
-gem 'mini_magick'
+gem 'rmagick'
 
 
 # Use jquery as the JavaScript library
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,8 +1,8 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -15,7 +15,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   version :thumb do
-    # process :crop
+    process :crop
     resize_to_fill(100, 100)
   end
 
